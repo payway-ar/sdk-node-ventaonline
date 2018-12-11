29794eee7d811c209590dfd6a31c466b3288005f
Marcos Volpi <mark.landia@gmail.com>
2018-12-11 10:01:01 -0300

    Fix

--- ejemplos/ejemplo_consola.js
+++ ejemplos/ejemplo_consola.js
@@ -559,7 +559,7 @@ function examplePaymentRequest(sdk) {
                 payment_type: "single",
                 sub_payments: [],
                 apiKey: "566f2c897b5e4bfaa0ec2452f5d67f13",
-                formSite: '00021625'
+                formSite: '00021625',
                 'Content-Type': "application/json"
             };
             var paymentData = new paymentMod.paymentData(args);
