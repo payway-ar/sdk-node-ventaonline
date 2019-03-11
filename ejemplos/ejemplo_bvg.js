ed372532386d222ff2cd90ce309903bad467e751
Marcos Volpi <mark.landia@gmail.com>
2019-03-11 16:32:47 -0300

    Añade pago con Decidir

--- ejemplos/ejemplo_bvg.js
+++ ejemplos/ejemplo_bvg.js
@@ -168,11 +168,189 @@ app.get('/pago_tp', function(req, res) {
             operationid: req.query.ord,
             publicKey: dataTransaction.publicRequestKey,
             merchant: dataGeneral.merchant,
-        }); 
-
-
-         
+        });   
     });
+});
+
+
+
+app.get('/pago_tp_action', function(req, res) {
+    if(req.query.success){
+        var status = "ACEPTADO";
+    }else{
+        var status = "RECHAZADO";
+    }
+
+    db.all(`UPDATE transaccion 
+        SET pagotpstatus='`+ status +`',
+        pagotpresponse='`+ req.query.formresponse +`'
+        WHERE id='`+ req.query.ord +`'`, (err, rows) => {
+        if (err) {
+          console.error(err.message);
+        }
+
+        res.render('./success.ejs');
+    }); 
+});
+
+app.get('/pago_decidir', function(req, res) {
+    db.get(`SELECT * FROM transaccion WHERE id=`+req.query.ord, (err, row) => {
+        if (err) {
+          console.error(err.message);
+        }          
+        var dataTransaction=JSON.parse(row.transactionresponse);
+        var dataGeneral = JSON.parse(row.data);
+        var dataTpResponse = JSON.parse(row.pagotpresponse);
+        /*
+        console.log(dataTransaction);
+        console.log(dataGeneral);
+        console.log(dataTpResponse);
+        */
+
+        if(dataTpResponse.TokenizationFlag == "true"){
+            var flag_token = "1";
+        }else{
+            var flag_token = "0";
+        }
+
+        if(dataTpResponse.SecurityCodeCheck == "true"){
+            var flag_code_check = "1";
+        }else{
+            var flag_code_check = "0";
+        }
+
+        /*
+        var args = {
+            data: {
+               "public_token":dataTpResponse.Token,
+               "volatile_encrypted_data":dataTpResponse.VOLATILE_ENCRYPTED_DATA,
+               "public_request_key":dataTransaction.publicRequestKey,
+               "issue_date":dataTpResponse.TokenDate,
+               "flag_security_code":flag_code_check,
+               "flag_tokenization":flag_token,
+               "flag_selector_key":dataTpResponse.SelectorClaveFlag,
+               "flag_pei":"1",
+               "card_holder_name":dataTpResponse.DatosAdicionales.nombre,
+               "card_number":"4507990000004905",
+               "card_expiration_month":"08",
+               "card_expiration_year":"19",
+               "card_holder_identification":{
+                  "type": dataTpResponse.DatosAdicionales.tipoDocumento,
+                  "number": dataTpResponse.DatosAdicionales.numeroDocumento
+               },
+               "fraud_detection":{
+                  "device_unique_identifier":"12345"
+               }
+            },
+            headers: {
+                apikey: dataGeneral.key_public,
+                'Content-Type': 'application/json'
+            }
+        };
+        */
+
+
+        
+        var args = {
+            data: {
+               "public_token":96291105,
+               "volatile_encrypted_data":"YRfrWggICAggsF0nR6ViuAgWsPr5ouR5knIbPtkN+yntd7G6FzN/Xb8zt6+QHnoxmpTraKphZVHvxA==",
+               "public_request_key":"5516e585-c0b6-447c-bbb0-8ee1fa75d8ca",
+               "issue_date":"20190108",
+               "flag_security_code":"0",
+               "flag_tokenization":"0",
+               "flag_selector_key":"1",
+               "flag_pei":"1",
+               "card_holder_name":"Comprador",
+               "card_number":"4507990000004905",
+               "card_expiration_month":"08",
+               "card_expiration_year":"19",
+               "card_holder_identification":{
+                  "type":"DNI",
+                  "number":"33222444"
+               },
+               "fraud_detection":{
+                  "device_unique_identifier":"12345"
+               }
+            },
+            headers: {
+                apikey: 'b192e4cb99564b84bf5db5550112adea',
+                'Content-Type': 'application/json'
+            }
+        };        
+        
+
+        var client = new Client();
+
+        client.post('https://developers.decidir.com/api/v2' + "/tokens", args, function(data, response) {
+            //resolve(data.id);
+            console.log(data);
+            //var dataResponse = JSON.parse(data);
+            console.log(data.id);
+
+
+            dataPago = {
+                "site_transaction_id": req.query.ord,
+                "payment_mode": "bsa",
+                "token": data.id,
+                "customer": {"id": dataGeneral.user_id, "email": dataGeneral.user_mail},
+                "payment_method_id": dataGeneral.decpaymentmethod,
+                //"amount": str_replace(",",".",dataGeneral.amount),
+                "amount": dataGeneral.amount.replace(",", "."),
+                "bin": dataGeneral.bin,
+                "currency": dataGeneral.currencydec,
+                "installments": dataGeneral.cuotas,
+                "description": "prueba",
+                "payment_type": "single",
+                "establishment_name": dataGeneral.establishment,
+                "sub_payments": []
+            }
+            client.post('https://developers.decidir.com/api/v2' + "/payments", args, function(data, response) {
+                //resolve(data.id);
+                console.log('data de pago::::');
+                console.log(data);
+
+                var execresponse;
+                if(data.status=="approved" || 1==1){
+                    var statusPago = "ACEPTADO";
+                    var execresponse = `{
+                        "id": `+ data.id + `,
+                        "status":`+  data.status + `,
+                        "ticket":`+  data.status_details.ticket + `,
+                        "authorization":`+  data.status_details.card_authorization_code + `,
+                        "amount":`+  data.currency + `,
+                        "date":`+  data.date + `
+                    }`;
+                    
+                                                  
+                }else{
+
+                    var statusPago="RECHAZADO";
+                }
+
+
+                db.all(`UPDATE transaccion 
+                    SET pagodecresponse='`+ execresponse +`',
+                    pagodecstatus='`+ statusPago +`'
+                    WHERE id='`+ req.query.ord +`'`, (err, rows) => {
+                    if (err) {
+                      console.error(err.message);
+                    }
+
+                    res.render('./success.ejs');
+                });                 
+            });
+
+
+
+        });
+
+        //console.log(dataInfo);
+
+    });
+
+    res.render('./success.ejs');
+
 });
 
 
