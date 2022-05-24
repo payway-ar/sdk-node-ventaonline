d4ecc29f0482bd9667a7e332e2d6cf3d88dc86ef
albocando <aocando@prismamp.com>
2022-05-24 10:11:56 -0300

    aplicacion de 0
    refactor nodeJs

--- ejemplos/SDK-Nodejs-BilleteraVirtualGateway-master/test/runAllTests.js
+++
@@ -1,4 +0,0 @@
-var transactionTest = require('./transactionTest');
-var discoverTest = require('./discoverTest');
-var pushNotificationTest = require('./pushNotificationTest');
-var getCredentialsTest = require('./getCredentialsTest');
