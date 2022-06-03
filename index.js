6140a96a18b8b1528d5a6adb381b181ec62b296e
albocando <aocando@prismamp.com>
2022-06-03 16:09:26 -0300

    fix union partial y refund

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ var sdk = new sdkModulo.sdk('developer', constants.PUBLIC_API_KEY, constants.PRI
 console.log("hola mundo");
 //exampleGetAllPayments(sdk);
 //exampleRefund(sdk);
+//examplePartialRefund(sdk);
 
 function exampleGetAllPayments(sdk) {
     var args = {
@@ -41,7 +42,7 @@ function exampleGetAllPayments(sdk) {
 };
 function exampleRefund(sdk){
     //SE HACE UN PAGO PARA PODER REALIZAR LA DEVOLUCIÓN
-        paymentId = 41359663;
+        paymentId = 41359688;
         var args = {
             data: {
     
@@ -60,6 +61,39 @@ function exampleRefund(sdk){
                 console.log("")
                 console.log("")
                 console.log("                  REFUND                 ");
+                console.log("-----------------------------------------");
+                console.log("refund result:");
+                console.log(resp);
+                console.log("-----------------------------------------");
+                console.log("refund error:");
+                console.log(err);
+                console.log("-------------------***-------------------");
+            });
+        }, 2000);
+};
+
+function examplePartialRefund(sdk){
+    //SE HACE UN PAGO PARA PODER REALIZAR LA DEVOLUCIÓN
+        paymentId = 41359719;
+        amount = 300;
+        var args = {
+            data: {
+                amount: amount
+            },
+            headers: {
+                "apikey": constants.PRIVATE_API_KEY,
+                "Content-Type": "application/json",
+                "Cache-Control": "no-cache"
+            }
+        };
+        setTimeout(function() {
+             refundMod.partialRefund(sdk, paymentId, args).then(function(resp) {
+                console.log("")
+                console.log("")
+                console.log("Reintegro monto total de la transacción")
+                console.log("")
+                console.log("")
+                console.log("                PARTIAL  REFUND                 ");
                 console.log("-----------------------------------------");
                 console.log("refund result:");
                 console.log(resp);
